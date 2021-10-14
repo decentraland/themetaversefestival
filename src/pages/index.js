ac78bfe102e863847516afd0453feee49f410de7
frami <i@2fd.me>
2021-10-14 12:39:13 -0500

    fix: add seo

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,14 +13,41 @@ import Merch from './../components/Merch';
 import Sponsors from './../components/Sponsors';
 import Footer from './../components/Footer';
 
+const seo = {
+  title: 'Metaverse Festival | Decentraland',
+  description: 'A four-day celebration of music, culture and creativity in the virtual social world of Decentraland, the Metaverse Festival is a grand collision of light, sound and portable toilets.',
+  image: '/festival.jpg',
+}
+
+const meta = {
+  ...seo,
+  'og:description': seo.description || '',
+  'twitter:description': seo.description || '',
+  'og:image': seo.image || '',
+  'twitter:image': seo.image || '',
+  'twitter:card': 'summary_large_image',
+  'twitter:creator': '@decentraland',
+  'og:type': 'website',
+}
 
 // markup
 const IndexPage = () => {
-
   return (
     <StyledIndexPage>
       <Helmet>
-        <title>Metaverse Festival | Decentraland | Testing</title>
+        {Object.keys(meta)
+          .filter((name) => Boolean(meta[name]))
+          .map((name) => {
+            if (name === 'title') {
+              return <title key={name}>{meta[name]}</title>
+            }
+
+            if (name.startsWith('og:')) {
+              return <meta key={name} property={name} content={meta[name]} />
+            }
+
+            return <meta key={name} name={name} content={meta[name]} />
+          })}
       </Helmet>
       <Layout>
         <header>
