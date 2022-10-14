f79a2d7d5bd788b80c64a70ae76d351fff7e78df
franzo <franciscoaboy@gmail.com>
2022-10-14 12:07:05 -0300

    analytics script

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React, { useState } from "react"
 import styled from 'styled-components'
 import { Helmet } from "react-helmet"
 import Layout from './../components/Layout'
+import { Script } from 'gatsby'
 import Hero from './../components/Hero'
 import Banner from "../components/Banner"
 
@@ -62,6 +63,18 @@ const IndexPage = () => {
           }
         </main>
       </Layout>
+      <Script id="analytics-script">
+        {
+          !function () {
+            var analytics = window.analytics = window.analytics || [];
+            if (!analytics.initialize) if (analytics.invoked) window.console && console.error && console.error("Segment snippet included twice."); else {
+              analytics.invoked = !0; analytics.methods = ["trackSubmit", "trackClick", "trackLink", "trackForm", "pageview", "identify", "reset", "group", "track", "ready", "alias", "debug", "page", "once", "off", "on", "addSourceMiddleware", "addIntegrationMiddleware", "setAnonymousId", "addDestinationMiddleware"]; analytics.factory = function (e) { return function () { var t = Array.prototype.slice.call(arguments); t.unshift(e); analytics.push(t); return analytics } }; for (var e = 0; e < analytics.methods.length; e++) { var key = analytics.methods[e]; analytics[key] = analytics.factory(key) } analytics.load = function (key, e) { var t = document.createElement("script"); t.type = "text/javascript"; t.async = !0; t.src = "https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js"; var n = document.getElementsByTagName("script")[0]; n.parentNode.insertBefore(t, n); analytics._loadOptions = e }; analytics._writeKey = "ymopugfGLGw3BBim7w92OeEs8BokW4To";; analytics.SNIPPET_VERSION = "4.15.3";
+              analytics.load("ymopugfGLGw3BBim7w92OeEs8BokW4To");
+              analytics.page();
+            }
+          }()
+        }
+      </Script>
     </StyledIndexPage>
   )
 }
