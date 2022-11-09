f09d6eac8b658898957665b148dea643e2f32068
fede-blanco <blancofedem@gmail.com>
2022-11-09 16:38:02 -0300

    Add days-data-2022.js

---
+++ utils/days-data-2022.js
@@ -0,0 +1,878 @@
+import React from "react";
+
+export const days = [
+  {
+    linkLabel: (
+      <>
+        <span className="day-tag">Thursday 10th</span>
+      </>
+    ),
+    date: "Thursday 10th",
+    stages: [
+      {
+        name: "MAIN STAGE",
+        acts: [
+          {
+            name: "THE STICKMEN PROJECT",
+            time: "23pm UTC",
+          },
+          {
+            name: "VLADIMIR CAUCHEMAR",
+            time: "22pm UTC",
+          },
+          {
+            name: "ERIKA KRALL",
+            time: "21pm UTC",
+          },
+          {
+            name: "HANDSHAKING",
+            time: "19pm UTC",
+          },
+        ],
+      },
+      {
+        name: "OZZFEST",
+        acts: [
+          {
+            name: "MOTORHEAD",
+            time: "23pm UTC",
+          },
+          {
+            name: "MEGADETH",
+            time: "22pm UTC",
+          },
+          {
+            name: "SKID ROW",
+            time: "21pm UTC",
+          },
+          {
+            name: "BLACK LABEL SOCIETY",
+            time: "20pm UTC",
+          },
+          {
+            name: "RAVEN AGE",
+            time: "19pm UTC",
+          },
+          {
+            name: "BRITOF",
+            time: "18pm UTC",
+          },
+        ],
+      },
+      {
+        name: "KRAKEN AFTER PARTY",
+        acts: [
+          {
+            name: "CJ TRAXX",
+            time: "0am UTC",
+          },
+        ],
+      },
+      {
+        name: "JERRY GARCIA",
+        acts: [
+          {
+            name: "MICHELE STODDART",
+            time: "21pm UTC",
+          },
+          {
+            name: "LOUIE BRENNAN",
+            time: "20pm UTC",
+          },
+          {
+            name: "COUNTERS CREEK",
+            time: "19pm UTC",
+          },
+          {
+            name: "KITCHMAN SCHMIDT",
+            time: "18pm UTC",
+          },
+          {
+            name: "STOMPING DAVE",
+            time: "17pm UTC",
+          },
+          {
+            name: "MA POLAINE",
+            time: "16pm UTC",
+          },
+        ],
+      },
+      {
+        name: "JAZZ CLUB",
+        acts: [
+          {
+            name: "ALFIE JAZZ VIDEOS",
+            time: "1am UTC",
+          },
+        ],
+      },
+      {
+        name: "OG COMMUNITY STAGE",
+        acts: [
+          {
+            name: "HARRISON FIRST",
+            time: "0am UTC",
+          },
+          {
+            name: "TRU CLUB",
+            time: "23pm UTC",
+          },
+          {
+            name: "GLITCH CANDIES",
+            time: "22:30pm UTC",
+          },
+          {
+            name: "CROMULON",
+            time: "22pm UTC",
+          },
+          {
+            name: "SHARK",
+            time: "21:30pm UTC",
+          },
+          {
+            name: "MARCOS NAIDE",
+            time: "21pm UTC",
+          },
+        ],
+      },
+      {
+        name: "DILLON FRANCIS",
+        acts: [
+          {
+            name: "DILLON FRANCIS",
+            time: "12pm UTC",
+          },
+          {
+            name: "DILLON FRANCIS",
+            time: "ALL DAY",
+          },
+        ],
+      },
+      {
+        name: "CASA AZUL STAGE",
+        acts: [
+          {
+            name: "THE GLOBO COLLECTIVE",
+            time: "ALL DAY",
+          },
+          {
+            name: "JOW",
+            time: "ALL DAY",
+          },
+          {
+            name: "THAMES HOUSE ORCESTRA",
+            time: "ALL DAY",
+          },
+        ],
+      },
+      {
+        name: "RANDOM RAVERS",
+        acts: [
+          {
+            name: "MAI LAWSON",
+            time: "0am UTC",
+          },
+          {
+            name: "GLÜCK",
+            time: "22:30pm UTC ",
+          },
+          {
+            name: "LOLU MENAYED",
+            time: "21pm UTC",
+          },
+          {
+            name: "SOUND PROCESS",
+            time: "19:30pm UTC",
+          },
+          {
+            name: "NACHO VARELA & CRUZ VITTOR",
+            time: "18pm UTC",
+          },
+        ],
+      },
+      {
+        name: "TRU BAND",
+        acts: [
+          {
+            name: "SHONTELLE",
+            time: "23pm UTC",
+          },
+          {
+            name: "PAT DIMITRI",
+            time: "22pm UTC ",
+          },
+          {
+            name: "BUFALO",
+            time: "20pm UTC",
+          },
+          {
+            name: "ASH ORPHAN",
+            time: "18pm UTC",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    linkLabel: (
+      <>
+        <span className="day-tag">Friday 11th</span>
+      </>
+    ),
+    date: "Friday 11th",
+    stages: [
+      {
+        name: "MAIN STAGE",
+        acts: [
+          {
+            name: "BRELAND",
+            time: "23pm UTC",
+          },
+          {
+            name: "SPOTIWIFI",
+            time: "22pm UTC",
+          },
+          {
+            name: "AMADIS AND THE AMBASSADORS",
+            time: "21pm UTC",
+          },
+          {
+            name: "MAIJA KAUHANEN",
+            time: "19pm UTC",
+          },
+        ],
+      },
+      {
+        name: "OZZFEST",
+        acts: [
+          {
+            name: "OZZY OSBOURNE",
+            time: "0am UTC",
+          },
+          {
+            name: "SKID ROW",
+            time: "23pm UTC",
+          },
+          {
+            name: "BLACK LABEL SOCIETY",
+            time: "22pm UTC",
+          },
+          {
+            name: "RAVEN AGE",
+            time: "20pm UTC",
+          },
+          {
+            name: "BRITOF",
+            time: "18pm UTC",
+          },
+        ],
+      },
+      {
+        name: "KRAKEN AFTER PARTY",
+        acts: [
+          {
+            name: "HARRISON FIRST",
+            time: "23:40pm UTC",
+          },
+        ],
+      },
+      {
+        name: "JERRY GARCIA",
+        acts: [
+          {
+            name: "OUR MAN IN THE FIELD",
+            time: "21pm UTC",
+          },
+          {
+            name: "JACK HARRIS",
+            time: "20pm UTC",
+          },
+          {
+            name: "ROB CORCORAN",
+            time: "19pm UTC",
+          },
+          {
+            name: "JAMES RILEY",
+            time: "18pm UTC",
+          },
+          {
+            name: "DRAIN AND FARRIS",
+            time: "17pm UTC",
+          },
+          {
+            name: "BUFFOS WAKE",
+            time: "16pm UTC",
+          },
+        ],
+      },
+      {
+        name: "JAZZ CLUB",
+        acts: [
+          {
+            name: "ANETA WARDELL",
+            time: "1am UTC",
+          },
+        ],
+      },
+      {
+        name: "OG COMMUNITY STAGE",
+        acts: [
+          {
+            name: "SERENA ELIS",
+            time: "0am UTC",
+          },
+          {
+            name: "ECLECTIC METHOD",
+            time: "23pm UTC",
+          },
+          {
+            name: "ALP",
+            time: "22:30pm UTC",
+          },
+          {
+            name: "BUFALO",
+            time: "22pm UTC",
+          },
+          {
+            name: "STONEY EYE",
+            time: "21:30pm UTC",
+          },
+          {
+            name: "GUCCI TOE",
+            time: "21pm UTC",
+          },
+        ],
+      },
+      {
+        name: "DILLON FRANCIS",
+        acts: [
+          {
+            name: "DILLON FRANCIS",
+            time: "12pm UTC",
+          },
+          {
+            name: "DILLON FRANCIS",
+            time: "ALL DAY",
+          },
+        ],
+      },
+      {
+        name: "CASA AZUL STAGE",
+        acts: [
+          {
+            name: "DESTA FRENCH",
+            time: "ALL DAY",
+          },
+          {
+            name: "GABRIEL MORENO",
+            time: "ALL DAY",
+          },
+          {
+            name: "DGSOLARIS JEREMY TUPLIN",
+            time: "ALL DAY",
+          },
+        ],
+      },
+      {
+        name: "RANDOM RAVERS",
+        acts: [
+          {
+            name: "UNTZ KING",
+            time: "20:30pm UTC",
+          },
+          {
+            name: "MERIO BRAS",
+            time: "19:45pm UTC ",
+          },
+          {
+            name: "0XPET3R",
+            time: "19pm UTC",
+          },
+        ],
+      },
+      {
+        name: "TRU BAND",
+        acts: [
+          {
+            name: "DOMINO",
+            time: "2am UTC",
+          },
+          {
+            name: "CROMULON",
+            time: "0am UTC ",
+          },
+          {
+            name: "AMBIENFT",
+            time: "19pm UTC",
+          },
+          {
+            name: "NIFTY SAX",
+            time: "18pm UTC",
+          },
+          {
+            name: "PHUTURE",
+            time: "17pm UTC",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    linkLabel: (
+      <>
+        <span className="day-tag">Saturday 12th</span>
+      </>
+    ),
+    date: "Saturday 12th",
+    stages: [
+      {
+        name: "MAIN STAGE",
+        acts: [
+          {
+            name: "AKIRA THE DON",
+            time: "23pm UTC",
+          },
+          {
+            name: "SOZI",
+            time: "22pm UTC",
+          },
+          {
+            name: "'2AM + PIP",
+            time: "21pm UTC",
+          },
+          {
+            name: "ATARASHII GAKKO ",
+            time: "12pm UTC",
+          },
+        ],
+      },
+      {
+        name: "OZZFEST",
+        acts: [
+          {
+            name: "MOTORHEAD",
+            time: "0am UTC",
+          },
+          {
+            name: "OZZY OSBOURNE",
+            time: "23pm UTC",
+          },
+          {
+            name: "MEGADETH",
+            time: "22pm UTC",
+          },
+          {
+            name: "BLACK LABEL SOCIETY",
+            time: "21pm UTC",
+          },
+          {
+            name: "BRITOF",
+            time: "18pm UTC",
+          },
+        ],
+      },
+      {
+        name: "KRAKEN AFTER PARTY",
+        acts: [],
+      },
+      {
+        name: "JERRY GARCIA",
+        acts: [
+          {
+            name: "LIVE RAINBOW FULL OF SOUND",
+            time: "0am UTC",
+          },
+          {
+            name: "FIONA BEVAN",
+            time: "21pm UTC",
+          },
+          {
+            name: "SMITH AND BREWER",
+            time: "20pm UTC",
+          },
+          {
+            name: "SOPHIE CRAWFORD",
+            time: "19pm UTC",
+          },
+          {
+            name: "MAZ O'CONNOR",
+            time: "18pm UTC",
+          },
+          {
+            name: "TRUCKSTOP HONEYMOON",
+            time: "17pm UTC",
+          },
+          {
+            name: "HANNAH NICHOLSON",
+            time: "16pm UTC",
+          },
+        ],
+      },
+      {
+        name: "JAZZ CLUB",
+        acts: [
+          {
+            name: "ELIANE CORREA",
+            time: "1am UTC",
+          },
+        ],
+      },
+      {
+        name: "OG COMMUNITY STAGE",
+        acts: [
+          {
+            name: "JACK FROST ALIEN DJ",
+            time: "0am UTC",
+          },
+          {
+            name: "AMBIE NFT",
+            time: "23pm UTC",
+          },
+          {
+            name: "BOBBY YAPS",
+            time: "22:30pm UTC",
+          },
+          {
+            name: "ELYSE",
+            time: "22pm UTC",
+          },
+          {
+            name: "MARTY GERBS",
+            time: "21:30pm UTC",
+          },
+          {
+            name: "LEO PASTEL",
+            time: "21pm UTC",
+          },
+        ],
+      },
+      {
+        name: "DILLON FRANCIS",
+        acts: [
+          {
+            name: "DILLON FRANCIS",
+            time: "12pm UTC",
+          },
+          {
+            name: "DILLON FRANCIS",
+            time: "ALL DAY",
+          },
+        ],
+      },
+      {
+        name: "CASA AZUL STAGE",
+        acts: [
+          {
+            name: "BALKAN HOT CLUB",
+            time: "ALL DAY",
+          },
+          {
+            name: "STEPHANIE SANTIAGO",
+            time: "ALL DAY",
+          },
+          {
+            name: "CHRISTALIS VELASCO",
+            time: "ALL DAY",
+          },
+        ],
+      },
+      {
+        name: "RANDOM RAVERS",
+        acts: [
+          {
+            name: "DEAD TONES",
+            time: "23:30pm UTC",
+          },
+          {
+            name: "KANA HISHIYA",
+            time: "22pm UTC ",
+          },
+          {
+            name: "RAMY MISHRIKY",
+            time: "20:30pm UTC",
+          },
+          {
+            name: "ARIVI",
+            time: "19pm UTC",
+          },
+        ],
+      },
+      {
+        name: "TRU BAND",
+        acts: [
+          {
+            name: "THE MUSICAL DOC",
+            time: "3am UTC",
+          },
+          {
+            name: "STONEY + KROOKED TREEZ",
+            time: "2am UTC ",
+          },
+          {
+            name: "THE PERRIS",
+            time: "0am UTC",
+          },
+          {
+            name: "JOHNY TIGER",
+            time: "22pm UTC",
+          },
+          {
+            name: "TOM TARNO",
+            time: "21pm UTC",
+          },
+          {
+            name: "MR. SWE",
+            time: "20pm UTC",
+          },
+          {
+            name: "MARCOS NAIDE",
+            time: "19pm UTC",
+          },
+          {
+            name: "JENNIFER RABHA",
+            time: "15pm UTC",
+          },
+          {
+            name: "KR",
+            time: "14:30pm UTC",
+          },
+          {
+            name: "GEMMI",
+            time: "14pm UTC",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    linkLabel: (
+      <>
+        <span className="day-tag">Sunday 13th</span>
+      </>
+    ),
+    date: "Sunday 13th",
+    stages: [
+      {
+        name: "MAIN STAGE",
+        acts: [
+          {
+            name: "BJÖRK",
+            time: "23pm UTC",
+          },
+          {
+            name: "LOSERS",
+            time: "22pm UTC",
+          },
+          {
+            name: "LIAN GOLD",
+            time: "21pm UTC",
+          },
+          {
+            name: "SNH48",
+            time: "12pm UTC",
+          },
+        ],
+      },
+      {
+        name: "OZZFEST",
+        acts: [
+          {
+            name: "SKID ROW",
+            time: "23pm UTC",
+          },
+          {
+            name: "MEGADETH",
+            time: "22pm UTC",
+          },
+          {
+            name: "MOTORHEAD",
+            time: "21pm UTC",
+          },
+          {
+            name: "MEGADETH",
+            time: "20pm UTC",
+          },
+          {
+            name: "RAVEN AGE",
+            time: "19pm UTC",
+          },
+          {
+            name: "BRITOF",
+            time: "18pm UTC",
+          },
+        ],
+      },
+      {
+        name: "KRAKEN AFTER PARTY",
+        acts: [],
+      },
+      {
+        name: "JERRY GARCIA",
+        acts: [
+          {
+            name: "PAUL MOSELEY",
+            time: "21pm UTC",
+          },
+          {
+            name: "JAKE MORELY",
+            time: "20pm UTC",
+          },
+          {
+            name: "DJANGO CHUTNEY",
+            time: "19pm UTC",
+          },
+          {
+            name: "LATEMOUTH BLAKE",
+            time: "18pm UTC",
+          },
+          {
+            name: "THE MAGIC LANTERN",
+            time: "17pm UTC",
+          },
+          {
+            name: "CYENFIN",
+            time: "16pm UTC",
+          },
+        ],
+      },
+      {
+        name: "JAZZ CLUB",
+        acts: [
+          {
+            name: "FLETCHER'S BREW",
+            time: "1am UTC",
+          },
+        ],
+      },
+      {
+        name: "OG COMMUNITY STAGE",
+        acts: [
+          {
+            name: "LOUIE C. RHYMES",
+            time: "23pm UTC",
+          },
+          {
+            name: "EMANATE",
+            time: "22:30pm UTC",
+          },
+          {
+            name: "DAO RECORDS",
+            time: "22pm UTC",
+          },
+          {
+            name: "KANE MAYFIELD",
+            time: "21:30pm UTC",
+          },
+          {
+            name: "JOSE CABRERAV",
+            time: "21pm UTC",
+          },
+        ],
+      },
+      {
+        name: "DILLON FRANCIS",
+        acts: [
+          {
+            name: "DILLON FRANCIS",
+            time: "12pm UTC",
+          },
+          {
+            name: "DILLON FRANCIS",
+            time: "ALL DAY",
+          },
+        ],
+      },
+      {
+        name: "CASA AZUL STAGE",
+        acts: [
+          {
+            name: "ABDOULAYE SAMB",
+            time: "ALL DAY",
+          },
+          {
+            name: "THREE CANE WHALE",
+            time: "ALL DAY",
+          },
+          {
+            name: "BALLADESTA",
+            time: "ALL DAY",
+          },
+        ],
+      },
+      {
+        name: "RANDOM RAVERS",
+        acts: [
+          {
+            name: "HERR",
+            time: "31:30pm UTC",
+          },
+          {
+            name: "IVAN POSEIDON",
+            time: "19:50pm UTC ",
+          },
+          {
+            name: "DRUNK AT VOGUE",
+            time: "18:20pm UTC",
+          },
+          {
+            name: "OMVRA",
+            time: "17pm UTC",
+          },
+        ],
+      },
+      {
+        name: "TRU BAND",
+        acts: [
+          {
+            name: "JACK FROST",
+            time: "4am UTC",
+          },
+          {
+            name: "BLK ORCHID",
+            time: "3am UTC ",
+          },
+          {
+            name: "BOBBY YAPS",
+            time: "2am UTC",
+          },
+          {
+            name: "DADA YUTE",
+            time: "1am UTC",
+          },
+          {
+            name: "F**K BOYS",
+            time: "0am UTC",
+          },
+          {
+            name: "KEVIN SOUSA & EMILY V",
+            time: "21pm UTC",
+          },
+          {
+            name: "THE HOLLOW LEGS",
+            time: "20pm UTC",
+          },
+          {
+            name: "V TORRES",
+            time: "19pm UTC",
+          },
+          {
+            name: "ZEAL LEVIN",
+            time: "18pm UTC",
+          },
+          {
+            name: "COCONUT SUNDAY",
+            time: "17pm UTC",
+          },
+          {
+            name: "ALMA",
+            time: "15pm UTC",
+          },
+          {
+            name: "X&ND",
+            time: "14pm UTC",
+          },
+        ],
+      },
+    ],
+  },
+];
