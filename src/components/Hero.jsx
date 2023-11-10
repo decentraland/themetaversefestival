7a489da76784e0400fbd10e6b56e206cfbebe755
lauti7 <lautigbustos@gmail.com>
2023-11-10 18:10:31 -0300

    rollback btn

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -45,10 +45,10 @@ const Hero = () => {
             </EventDataItem>
             <EventDataItem>
               <SaveTheDate
-                href="https://decentraland.org/blog/announcements/the-ultimate-guide-to-decentraland-music-festival-23"
+                href="https://events.decentraland.org/event/?id=97aff492-b4b9-4ba1-bd08-bfa7e32be036"
                 target="_blank"
               >
-                <p style={{ fontSize: "20px" }}>Ultimate Guide</p>{" "}
+                <p style={{ fontSize: "20px" }}>RSVP</p>{" "}
                 <Polygon src={polygon} />
               </SaveTheDate>
             </EventDataItem>
