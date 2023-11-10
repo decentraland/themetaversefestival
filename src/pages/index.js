afeb3740e85440310155e67543eb1e2ff2b26cf4
lauti7 <lautigbustos@gmail.com>
2023-11-10 10:51:22 -0300

    favicon

--- src/pages/index.js
+++ src/pages/index.js
@@ -63,22 +63,22 @@ const IndexPage = () => {
 
             return <meta key={name} name={name} content={meta[name]} />;
           })}
-        <link
+        {/* <link
           rel="apple-touch-icon"
           sizes="180x180"
           href="/apple-touch-icon.png"
-        />
+        /> */}
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="/favicon-32x32.png"
+          href="/favicon-23.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="/favicon-16x16.png"
+          href="/favicon-23.png"
         />
         <link rel="manifest" href="/site.webmanifest" />
         <meta name="msapplication-TileColor" content="#da532c" />
